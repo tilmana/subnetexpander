2b2c90d3f5b4a764e1795395ef13d16101549828
Arthur <59236083+tilmana@users.noreply.github.com>
2024-08-07 10:28:59 -0400

    Update subnetexpander.py

--- subnetexpander.py
+++ subnetexpander.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description="Get IPs from one or more subnets p
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-s', '--subnet', help='Subnet to pass directly')
 group.add_argument('-f', '--file', help='File to read subnet(s) from')
+parser.add_argument('-c,', '--csv', help='Defines whether file type is separated by commas versus newlines', action="store_true")
 
 args = parser.parse_args()
 
@@ -17,9 +18,16 @@ if args.subnet:
 if args.file:
     subnets = []
     f1 = open(args.file, "r")
-    f1lines = f1.readlines()
-    for line in f1lines:
-        subnets.append(line.strip("\n"))
+    if args.csv:
+        f1line = f1.readline()
+        f1lines = f1line.replace(" ", "").strip("\n").split(",")
+        for line in f1lines:
+            subnets.append(line)
+    else:
+        f1lines = f1.readlines()
+        for line in f1lines:
+            line = line.strip("\n")
+            subnets.append(line)
     for subnet in subnets:
         ips = [str(ip) for ip in ipaddress.IPv4Network(subnet)]
         for ip in ips:
